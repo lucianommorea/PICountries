4fdca42aa19ee719af9d44dac0c65773fa8f2289
Luciano <lucianommorea@gmail.com>
2022-12-12 20:51:53 -0300

    default axios deploy

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,8 @@ import axios from 'axios';
 import dotenv from 'dotenv';
 dotenv.config()
 
-axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
+axios.defaults.baseURL = 'https://picountries-production-a145.up.railway.app/'
+// axios.defaults.baseURL = "http://localhost:3001"
 
 
 ReactDOM.render(
